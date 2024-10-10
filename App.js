f90985bcb9a4ccb1863aa3351eb94f4079733124
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-10 15:37:44 +0300

    feat: list added

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import Header from './components/Header';
 
 export default function App() {
+  const [listOfItems, setListOfItems] = useState([
+    { text: 'Buy milk', index: 1 },
+    { text: 'Buy fruits', index: 1 },
+    { text: 'Wash the car', index: 1 },
+    { text: 'Writhe some code', index: 1 },
+    { text: 'Meet friends', index: 1 },
+  ]);
+
   return (
     <View>
       <Header />
+      <View>
+        <FlatList
+          data={listOfItems}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+        />
+      </View>
     </View>
   );
 }
