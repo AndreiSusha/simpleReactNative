bb6d83621bad69f5672d5badc90260267768d829
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-10 15:13:55 +0300

    feat: stiles added

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View>
+      <Header />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
--- components/Header.js
+++ components/Header.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function Header() {
-  return;
-  <View style={styles.main}>
-    <Text style={styles.text}>To do list</Text>
-  </View>;
+  return (
+    <View style={styles.main}>
+      <Text style={styles.text}>To do list</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   main: {
-    paddingTop: 50,
+    paddingTop: 40,
     height: 100,
     backgroundColor: '#00C49F',
   },
   text: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     color: 'white',
     fontStyle: 'italic',
